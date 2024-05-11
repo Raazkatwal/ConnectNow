8f37321e6ac6ef5108408f8a630088016f673f4b
Raaz <raazkatwal5@gmail.com>
2024-05-11 23:23:10 +0545

    created login page

--- index.php
+++ index.php
@@ -3,19 +3,18 @@
 <head>
     <?php include 'links.php' ?>
     <title>ConnectNow</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
     <?php include 'nav.php' ?>
-    <main>
-        <div class="main-content">
-            <div class="slogan-section">
-                <h1 class="slogan">Bringing You Closer, One Call at a Time</h1>
-                <button class="main-btn"><a href="login.php" class="main-btn">Get started <i class="fa-solid fa-chevron-right"></i></a></button>
-            </div>
-            <div class="graphics-section">
-                <div class="blob"></div>
-                <i class="fa-solid fa-video"></i>
-            </div>
+    <main class="main-content">
+        <div class="slogan-section">
+            <h1 class="slogan">Bringing You Closer, One Call at a Time</h1>
+            <button class="main-btn"><a href="login.php" class="main-btn">Get started <i class="fa-solid fa-chevron-right"></i></a></button>
+        </div>
+        <div class="graphics-section">
+            <div class="blob"></div>
+            <i class="fa-solid fa-video"></i>
         </div>
     </main>
 </body>
---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'links.php' ?>
+    <link rel="stylesheet" href="login.css">
+    <title>Login | ConnectNow</title>
+</head>
+<body>
+    <?php include 'nav.php' ?>
+    <main class="main-content">
+        <div class="message-section">
+            <h1>Welcome back !</h1>
+            <p>Enter your ID and Password to continue</p>
+        </div>
+        <form action="">
+            <h1>Login</h1>
+            <input type="email" name="" id="" placeholder="Enter your Email here">
+            <input type="password" name="" id="" placeholder="Enter your Password here">
+            <input type="submit" value="Login">
+            <p class="linktext">Dont have an account ? <a href="">Click here.</a></p>
+        </form>
+    </main>
+</body>
+</html>
