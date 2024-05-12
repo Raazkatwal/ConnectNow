0bf7369ce764ffbabbf4c3b2bfadb49aad94b60b
Raaz <raazkatwal5@gmail.com>
2024-05-12 18:25:37 +0545

    Created Signup page

---
+++ signup.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'links.php' ?>
+    <link rel="stylesheet" href="login.css">
+    <title>Sign up | ConnectNow</title>
+</head>
+<body>
+    <?php include 'nav.php' ?>
+    <main class="main-content">
+        <div class="message-section">
+            <h1>Join Us Now!</h1>
+            <p>Unlock a World of Possibilities</p>
+        </div>
+        <form action="">
+            <h1>Sign Up</h1>
+            <input type="text" name="" id="" placeholder="Enter your Full Name here">
+            <input type="email" name="" id="" placeholder="Enter your Email here">
+            <input type="password" name="" id="" placeholder="Enter your Password here">
+            <input type="submit" value="Sign up">
+            <p class="linktext">Already have an account ? <a href="login.php">Click here.</a></p>
+        </form>
+    </main>
+</body>
+</html>
